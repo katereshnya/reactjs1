98efc7aa336337885349c81042f86fcee2a6b47a
katereshnya <katereshnya@gmail.com>
2022-03-26 23:00:03 +0300

    lesson-3

---
+++ lesson-3/reactjs/src/components/message-list/message-list.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Input, InputAdornment } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import { Message } from "./message";
+import { useStyles } from "./use-styles";
+
+export const MessageList = () => {
+  const ref = useRef();
+
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      author: "Bot",
+      message: "message 333",
+      date: "date",
+    },
+  ]);
+
+  const styles = useStyles();
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTo(0, ref.current.scrollHeight);
+    }
+    const textInput = document.querySelector("input");
+    textInput.focus();
+  }, [messages]);
+
+  const sendMessage = () => {
+    if (value) {
+      setMessages([
+        ...messages,
+        {
+          author: "User",
+          message: value,
+          date: new Date().toLocaleDateString(),
+        },
+      ]);
+      setValue("");
+    }
+  };
+
+  const handlePressInput = ({ code }) => {
+    if (code === "Enter") {
+      sendMessage();
+    }
+  };
+
+  useEffect(() => {
+    const lastMessages = messages[messages.length - 1];
+    let timerId = null;
+
+    if (messages.length && lastMessages.author === "User") {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          {
+            author: "Bot",
+            message: "hello from bot",
+            date: new Date().toLocaleDateString(),
+          },
+        ]);
+      }, 500);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [messages]);
+
+  return (
+    <>
+      <div ref={ref}>
+        {messages.map((message, index) => (
+          <Message message={message} key={message.date} />
+        ))}
+      </div>
+
+      <Input
+        placeholder="Введите сообщение ..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyPress={handlePressInput}
+        className={styles.input}
+        fullWidth
+        endAdornment={
+          <InputAdornment position="end">
+            {value && <Send className={styles.icon} onClick={sendMessage} />}
+          </InputAdornment>
+        }
+        autoFocus={true}
+      />
+    </>
+  );
+};
